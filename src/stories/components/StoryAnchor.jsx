327b35f76944979d6e107401d54ad0b372a93ee1
Ryan <rmchenry@minitab.com>
2020-12-04 16:30:54 -0600

    fixing story anchor str replace logic

--- src/stories/components/StoryAnchor.jsx
+++ src/stories/components/StoryAnchor.jsx
@@ -2,6 +2,6 @@ import { Anchor } from "@storybook/addon-docs/blocks";
 
 export const StoryAnchor = ({ name = "", path = "" }) => (
   <Anchor
-    storyId={`${path.replace("/", "-").toLowerCase()}--${name.toLowerCase()}`}
+    storyId={`${path.replaceAll("/", "-").toLowerCase()}--${name.replaceAll(" ", "-").toLowerCase()}`}
   />
 );
